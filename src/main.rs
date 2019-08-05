9ddd4e624aee422e6bf7bef3eeaef969e2511c8f
Johannes Bauer <jb@jbr.fritz.box>
2019-08-05 19:17:47 +0200

    added list existing help files & change to toml format

--- src/main.rs
+++ src/main.rs
@@ -19,16 +19,18 @@ const HELP_FILE_TEXT: &'static str = "fromGetHelpEntries__ Command  Description\
 fn main() -> Result<(), std::io::Error> {
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
     let mut stderr = StandardStream::stderr(ColorChoice::Always);
+    let current_path = PathBuf::from(env::current_exe()?.parent().unwrap());
 
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
         stderr.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
         writeln!(&mut stderr, "Error: no Arguments found!")?;
+        stderr.set_color(ColorSpec::new().set_fg(Some(Color::White)))?;
+        print_existing_helpfiles(&current_path)?;
         exit(1);
     }
 
     // let current_path = env::current_dir()?;
-    let current_path = PathBuf::from(env::current_exe()?.parent().unwrap());
     let mut file_path = current_path.clone();
     let mut file_name = PathBuf::from(&args[1]);
 
@@ -41,6 +43,9 @@ fn main() -> Result<(), std::io::Error> {
 
     let file = File::open(file_path)?;
     let file_lines = std::io::BufReader::new(file);
+    let mut counter = 0_usize;
+
+    let mut input = String::new();
 
     for line in file_lines.lines() {
         match line {
@@ -52,11 +57,33 @@ fn main() -> Result<(), std::io::Error> {
                     stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)))?;
                     println!("{}", line);
                 }
+                counter += 1;
+                if counter == 24 {
+                    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Cyan)))?;
+                    write!(&mut stdout, "  -----------------------------------------------------------------------------\\/\n")?;
+                    std::io::stdin().read_line(&mut input)?;
+                    counter = 0;
+                }
             }
             Err(err) => {
                 writeln!(&mut stderr, "{:?}", err)?;
             }
         }
+    }
+
+    Ok(())
+}
+
+fn print_existing_helpfiles(path: &PathBuf) -> Result<(), std::io::Error> {
+    for entry in std::fs::read_dir(path)? {
+        // let filename = ;
+        println!(
+            "{}",
+            entry?
+                .file_name()
+                .into_string()
+                .expect("Error reading existing helpfiles!: ")
+        );
     }
 
     Ok(())
