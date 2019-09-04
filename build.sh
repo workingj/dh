3f7562d58e41b954b19e8206124465158c50ca13
wj <workingJ@users.noreply.github.com>
2019-09-04 11:02:23 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "build release & copy binary & cargo clean"
-name="$(pwd)"					# get the current path as variable
-name="${name%"${name##*[!/]}"}" # extglob-free multi-trailing-/ trim
-name="${name##*/}"              # remove everything before the last /
-cargo build --release
-cp $CARGO_TARGET_DIR/release/$name ~/.jbtools/dh -v # copy the binary to th tools folder
-# sudo cp target/release/$name /usr/local/bin/ -v # copy the binary to th tools folder
-# cargo clean
